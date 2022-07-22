63c333523d7fdeb395e4cbfc0f3589dd4af1ce0c
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-07-22 23:49:26 +0300

    Add run_sql fuction to main() function

--- sqlrunner/__main__.py
+++ sqlrunner/__main__.py
@@ -1,6 +1,8 @@
 import argparse
 import pathlib
 
+import run_sql
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -11,13 +13,13 @@ def parse_args():
         help="String of database connection",
     )
     parser.add_argument(
-        "--input_file",
+        "--input_sql_file",
         required=True,
         type=pathlib.Path,
         help="Path to the input SQL file",
     )
     parser.add_argument(
-        "--output_file",
+        "--output_csv_file",
         required=True,
         type=pathlib.Path,
         help="Path to the output CSV file",
@@ -26,11 +28,19 @@ def parse_args():
 
 
 def main():
+    # Parse arguments
     args = parse_args()
     database_connection = args.database_connection
-    input_file = args.input_file
-    output_file = args.output_file
-    print(database_connection, input_file, output_file)
+    input_sql_file = args.input_sql_file
+    output_csv_file = args.output_csv_file
+
+    # Run SQL
+    output = run_sql.run_sql(
+        database_connection=database_connection, input_sql_file=input_sql_file
+    )
+
+    # Write CSV file (here just print all arguments for now)
+    print(output, output_csv_file)
 
 
 if __name__ == "__main__":
