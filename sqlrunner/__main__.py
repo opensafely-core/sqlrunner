d55f8c94330fc7be4fec6e1fbb94f943febc6fae
Iain Dillingham <iain@dillingham.me.uk>
2022-08-10 17:54:43 +0100

    Tidy up __main__.py
    We've cut this right back!

--- sqlrunner/__main__.py
+++ sqlrunner/__main__.py
@@ -2,11 +2,6 @@ from sqlrunner import main
 
 
 args = main.parse_args()
-url = args.url
-input_file = args.input_file
-output_file = args.output_file
-
-sql_query = main.read_text(input_file)
-results = main.run_sql(url=url, sql_query=sql_query)
-
-main.write_results(results, output_file)
+sql_query = main.read_text(args.input_file)
+results = main.run_sql(url=args.url, sql_query=sql_query)
+main.write_results(results, args.output_file)
