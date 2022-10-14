002fb5343efde6706c2d1ce4fcd6379d22de0f0d
Iain Dillingham <iain@dillingham.me.uk>
2022-10-14 17:22:53 +0100

    Add `--dummy-data-file` argument to CLI
    At present, an SQL Runner action breaks a local run (e.g. `opensafely
    run run_all`, because it cannot connect to a database. This makes it
    hard to test a pipeline that includes an SQL Runner action.
    
    By adding the `--dummy-data-file` argument to the CLI, we make it easier
    to test such a pipeline. Whilst a user must create a dummy data file
    themselves, they can nevertheless check that any downstream actions,
    which would otherwise expect an output from the SQL Runner action, work
    as expected.
    
    Closes #32

--- sqlrunner/__main__.py
+++ sqlrunner/__main__.py
@@ -6,5 +6,8 @@ from sqlrunner import main
 
 args = main.parse_args(sys.argv[1:], os.environ)
 sql_query = main.read_text(args.input)
-results = main.run_sql(dsn=args.dsn, sql_query=sql_query)
+if args.dummy_data_file is None:
+    results = main.run_sql(dsn=args.dsn, sql_query=sql_query)
+else:
+    results = args.dummy_data_file
 main.write_results(results, args.output)
