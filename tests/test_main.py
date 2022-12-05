cc1b64f3ec2b7703f2ced56293758c4d67b67f0b
Iain Dillingham <iain@dillingham.me.uk>
2022-12-05 11:28:44 +0000

    Merge tests
    `test_write_results` and `test_write_results_with_zero_results` are the
    same test, but with different parameters. If we parametrize them, then
    we can merge them.

--- tests/test_main.py
+++ tests/test_main.py
@@ -80,26 +80,22 @@ def test_run_sql(mssql_database):
     assert results == [{"patient_id": 1}]
 
 
-def test_write_results(tmp_path):
+@pytest.mark.parametrize(
+    "results,csv_string",
+    [
+        ([{"id": 1}, {"id": 2}], "id\n1\n2\n"),
+        ([], ""),  # zero results
+    ],
+)
+def test_write_results(tmp_path, results, csv_string):
     # arrange
     f_path = tmp_path / "results.csv"
 
     # act
-    main.write_results([{"id": 1}, {"id": 2}], f_path)
+    main.write_results(results, f_path)
 
     # assert
-    assert f_path.read_text(encoding="utf-8") == "id\n1\n2\n"
-
-
-def test_write_results_with_zero_results(tmp_path):
-    # arrange
-    f_path = tmp_path / "results.csv"
-
-    # act
-    main.write_results([], f_path)
-
-    # assert
-    assert f_path.read_text(encoding="utf-8") == ""
+    assert f_path.read_text(encoding="utf-8") == csv_string
 
 
 @pytest.mark.parametrize(
