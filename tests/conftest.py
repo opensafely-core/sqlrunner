581f5e717d784af7238580ecc907df6a793a9576
Iain Dillingham <iain@dillingham.me.uk>
2023-07-31 15:12:43 +0100

    Add `log_output` fixture
    This is structlog's suggested approach for capturing log output. For
    more information, see: https://www.structlog.org/en/stable/testing.html.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import structlog
+from structlog.testing import LogCapture
 
 from .databases import make_mssql_database, wait_for_database
 from .docker import Containers
@@ -14,3 +16,13 @@ def mssql_database(containers):
     database = make_mssql_database(containers)
     wait_for_database(database)
     yield database
+
+
+@pytest.fixture
+def log_output():
+    return LogCapture()
+
+
+@pytest.fixture(autouse=True)
+def configure_structlog(log_output):
+    structlog.configure(processors=[log_output])
