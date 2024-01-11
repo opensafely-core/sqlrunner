25cef026d21d55ffe7c50cfada09e51c1da6c6e0
Jon Massey <jon.massey@thedatalab.org>
2024-01-11 16:16:01 +0000

    Update sqlrunner/main.py
    Co-authored-by: Iain Dillingham <iaindillingham@users.noreply.github.com>

--- sqlrunner/main.py
+++ sqlrunner/main.py
@@ -134,6 +134,6 @@ def write_results(results, destination):
 
 
 def read_dummy_data_file(f_path):
-    with f_path.open("r") as f:
+    with f_path.open("r", newline="") as f:
         reader = csv.DictReader(f)
         yield from reader
