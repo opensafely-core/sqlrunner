72e33ef07938887cc46b9e36949352ce01bbec80
Peter Inglesby <peter.inglesby@gmail.com>
2024-12-23 16:58:42 +0000

    Revert "feat: Switch to inclusion table for T1OO handling"
    This reverts commit a92ed3335ceed932f935c63e96e49ff19a3a0a9e.

--- sqlrunner/__init__.py
+++ sqlrunner/__init__.py
@@ -3,7 +3,4 @@ from pathlib import Path
 
 __version__ = Path(__file__).parent.joinpath("VERSION").read_text().strip()
 
-
-# This table has its name for historical reasons, and reads slightly oddly: it should be
-# interpreted as "allowed patients with regard to type one dissents"
-T1OOS_TABLE = "AllowedPatientsWithTypeOneDissent"
+T1OOS_TABLE = "PatientsWithTypeOneDissent"
